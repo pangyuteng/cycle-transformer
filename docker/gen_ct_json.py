1ad3a51fe632b23badb08ab3d6546dc9befc7c86
pangyuteng <pteng@redlradbei05920.ad.medctr.ucla.edu>
2025-11-10 14:06:27 -0800

    wip

--- docker/gen_ct_json.py
+++ docker/gen_ct_json.py
@@ -2,9 +2,14 @@
 import os
 import sys
 import json
+import pandas as pd
 from pathlib import Path
 
 root = sys.argv[1]
+
+train_csv_file = sys.argv[2]
+df = pd.read_csv(train_csv_file)
+
 file_list = [str(x) for x in Path(root).rglob("*")]
 file_list = [x for x in file_list if "DICOMDIR" not in x and not os.path.isdir(x)]
 
@@ -14,13 +19,20 @@ for category in category_list:
     if category not in mydict.keys():
         mydict[category] = []
     for file_path in file_list:
+        if any([x in file_path for x in df.train.to_list()]) is False:
+            continue
         if category in file_path:
             mydict[category].append(file_path)
 
-with open('ct_data.json','w') as f:
+with open('ct_data_train.json','w') as f:
     f.write(json.dumps(mydict, indent=4, sort_keys=True))
 
 """
-python gen_ct_json.py /radraid/pteng-public/Coltea-Lung-CT-100W/Coltea-Lung-CT-100W
+
+python gen_ct_json.py \
+    /radraid/pteng-public/Coltea-Lung-CT-100W/Coltea-Lung-CT-100W \
+    /radraid/pteng-public/Coltea-Lung-CT-100W/train_data.csv
+
+cp ct_data_train.json /radraid/pteng-public/Coltea-Lung-CT-100W
 
 """
